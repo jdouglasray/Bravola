9e92e11c29f83f84397281b7acee957e33dd223d
jdouglasray <33317724+jdouglasray@users.noreply.github.com>
2017-11-14 13:03:57 -0500

    Add files via upload

--- Bravola/CharCreator.java
+++ Bravola/CharCreator.java
@@ -92,6 +92,8 @@ public class CharCreator
 {
   public static void main(String[] args)
   {
+    String newline = System.getProperty("line.separator");
+
     Scanner in = new Scanner(System.in);
 
     PChar Player = new PChar();
@@ -107,62 +109,187 @@ public class CharCreator
 
     System.out.println();
 
-    System.out.println("What is your race?");
-    System.out.println("1. Human");
-    System.out.println("2. Smith");
-    System.out.println("3. Space Elves");
-    System.out.println("4. Gnarp'i");
-    System.out.println("5. Android");
-    System.out.println("6. Butlar");
-    System.out.print("> ");
+    Boolean choosingRace = true;
+    while (choosingRace == true)
+    {
+      System.out.println("What is your race?");
+      System.out.println("1. Human");
+      System.out.println("2. Smith");
+      System.out.println("3. Space Elves");
+      System.out.println("4. Gnarp'i");
+      System.out.println("5. Android");
+      System.out.println("6. Butlar");
+      System.out.print("> ");
 
-    choice = in.nextInt();
+      int choice = in.nextInt();
 
-    if (choice == 1)
-    {
-      System.out.println("You have chosen HUMAN.");
-      System.out.println("HUMANS are the natives of planet Earth. They are\n
-                          versatile, and have no stat bonuses or penalties.");
+      if (choice == 1)
+      {
+        System.out.println("You have chosen HUMAN.");
+        System.out.println("HUMANS are the natives of planet Earth. They are" + newline +
+                            "versatile, and have no stat bonuses or penalties.");
+        System.out.println("Would you like to choose this race?");
+        System.out.println("1. YES  2. NO");
+        System.out.print("> ");
+
+        choice = in.nextInt();
+
+        if (choice == 1)
+        {
+          Player.race = "Human"
+          choosingRace = false;
+        }
+        if (choice == 2)
+        {
+          break;
+        }
+        else
+        {
+          System.out.println("Invalid input.");
+        }
+      }
+      else if (choice == 2)
+      {
+        System.out.println("You have chosen SMITH.");
+        System.out.println("Members of the SMITH race have been given a bad" + newline +
+                          "lot in life. They have a penalty to all stats.");
+
+        System.out.println("Would you like to choose this race?");
+        System.out.println("1. YES  2. NO");
+        System.out.print("> ");
+
+        choice = in.nextInt();
+
+        if (choice == 1)
+        {
+          Player.race = "Smith"
+          choosingRace = false;
+        }
+        if (choice == 2)
+        {
+          break;
+        }
+        else
+        {
+          System.out.println("Invalid input.");
+        }
+
+        Player.str = 1;
+        Player.vit = 1;
+        Player.kno = 1;
+        Player.dex = 1;
+        Player.cha = 1;
+        Player.per = 1;
+      }
+      else if (choice == 3)
+      {
+        System.out.println("You have chosen SPACE ELVES.");
+        System.out.println("SPACE ELVES are native to the planet of North Poal," + newline +
+                            "a world near the galactic boundary. They have a bonus" + newline +
+                            "to CHARISMA. Being naturally thin, they have a penalty" + newline +
+                            "to STRENGTH.");
+
+        System.out.println("Would you like to choose this race?");
+        System.out.println("1. YES  2. NO");
+        System.out.print("> ");
+
+        choice = in.nextInt();
+
+
+        if (choice == 2)
+        {
+          break;
+        }
+        else
+        {
+          System.out.println("Invalid input.");
+        }
+
+        Player.cha = 14;
+        Player.str = 6;
+      }
+      else if (choice == 4)
+      {
+        System.out.println("You have chosen GNARP'I");
+        System.out.println("The GNARP'I are insectoid entities from the planet of" + newline +
+                            "Slimia. They have an appearance resembling a giant" + newline +
+                            "snail. They have a bonus to PERCEPTION. Lacking any" + newline +
+                            "skeleton, they have a penalty to VITALITY.");
+
+        System.out.println("Would you like to choose this race?");
+        System.out.println("1. YES  2. NO");
+        System.out.print("> ");
+
+        choice = in.nextInt();
+
+        if (choice == 2)
+        {
+          break;
+        }
+        else
+        {
+          System.out.println("Invalid input.");
+        }
+
+        Player.per = 14;
+        Player.vit = 6;
+      }
+      else if (choice == 5)
+      {
+        System.out.println("You have chosen ANDROID.");
+        System.out.println("The ANDROIDS are synthetic men and women created long" + newline +
+                            "ago by the HUMANS. Being made of plastics and metals," + newline +
+                            "they have a bonus to VITALITY. With a human veneer" + newline +
+                            "not quite convincing, they have a penalty to CHARISMA.");
+
+        System.out.println("Would you like to choose this race?");
+        System.out.println("1. YES  2. NO");
+        System.out.print("> ");
+
+        choice = in.nextInt();
+
+        if (choice == 2)
+        {
+          break;
+        }
+        else
+        {
+          System.out.println("Invalid input.");
+        }
+
+        Player.vit = 14;
+        Player.cha = 6;
+      }
+      else if (choice == 6)
+      {
+        System.out.println("You have chosen BUTLAR.");
+        System.out.println("The BUTLAR are a race native to the ocean world of" + newline +
+                            "PROTEEN. Known for creating stones out of sand with" + newline +
+                            "only the strength of their physique, the BUTLAR have" + newline +
+                            "a bonus to STRENGTH. Being naturally unwieldy, they" + newline +
+                            "have a penalty to DEXTERITY.");
+
+        System.out.println("Would you like to choose this race?");
+        System.out.println("1. YES  2. NO");
+        System.out.print("> ");
+
+        choice = in.nextInt();
+
+        if (choice == 2)
+        {
+          break;
+        }
+        else
+        {
+          System.out.println("Invalid input.");
+        }
+
+        Player.str = 14;
+        Player.dex = 6;
+      }
     }
-    else if (choice == 2)
-    {
-      System.out.println("You have chosen SMITH.");
-      System.out.println("Members of the SMITH race have been given a bad\n
-                          lot in life. They have a penalty to all stats.");
-    }
-    else if (choice == 3)
-    {
-      System.out.println("You have chosen SPACE ELVES.");
-      System.out.println("SPACE ELVES are native to the planet of North Poal,\n
-                          a world near the galactic boundary. They have a bonus\n
-                          to CHARISMA. Being naturally thin, they have a penalty\n
-                          to STRENGTH.");
-    }
-    else if (choice == 4)
-    {
-      System.out.println("You have chosen GNARP'I");
-      System.out.println("The GNARP'I are insectoid entities from the planet of\n
-                          Slimia. They have an appearance resembling a giant\n
-                          snail. They have a bonus to PERCEPTION. Lacking any\n
-                          skeleton, they have a penalty to VITALITY.");
-    }
-    else if (choice == 5)
-    {
-      System.out.println("You have chosen ANDROID.");
-      System.out.println("The ANDROIDS are synthetic men and women created long\n
-                          ago by the HUMANS. Being made of plastics and metals,\n
-                          they have a bonus to VITALITY. With a human veneer\n
-                          not quite convincing, they have a penalty to CHARISMA.")
-    }
-    else if (choice == 6)
-    {
-      System.out.println("You have chosen BUTLAR.");
-      System.out.println("The BUTLAR are a race native to the ocean world of\n
-                          PROTEEN. Known for creating stones out of sand with\n
-                          only the strength of their physique, the BUTLAR have\n
-                          a bonus to STRENGTH. Being naturally unwieldy, they\n
-                          have a penalty to DEXTERITY.")
-    }
+
+
 
 
 
