3276c8e407287a4005df4bb15be8ff37756be225
ZJBXIII <33867030+ZJBXIII@users.noreply.github.com>
2017-12-03 13:11:10 -0600

    Add files via upload
    wip combat / integrating stats and weapons and skills

---
+++ fightgame.java
@@ -0,0 +1,142 @@
+import java.util.Scanner;
+
+import java.util.Random;
+
+
+class Fighter
+{
+    Random rand = new Random(); 
+    int hp = 50; int spd = 5; int spdcountdown = 0; String name = "Blooper";
+    
+    //offensive categories
+    int strength = 10;int damage =rand.nextInt(1+(strength/2))+1;
+    
+    //defensive categories
+    int def = 5; int dodge=0;int block = 0;
+    int dodge_(int def , int spd){
+        dodge =rand.nextInt(1+(def/2) + (dexterity/4 +spd/4));
+        return dodge;
+    }
+    int block_(int defcalc , int strengthcalc)
+    {
+        dodge =rand.nextInt(1+(defcalc/2) + (strengthcalc/4));
+        return dodge;
+    }
+    
+    
+    
+
+    //"support" categories
+    int dexterity = 10; int accuracy = 0; //Also weapon skill maybe?
+    int accuracy_(int dexterity){
+        accuracy =rand.nextInt(1+(dexterity/2))+1;
+        return dodge;
+    }
+    
+    
+}
+
+class weapon
+{
+
+}
+class combat
+{
+    Scanner s = new Scanner(System.in);
+    boolean inCombat = true;
+    Random rand = new Random();
+    int attack (Fighter blooper, Fighter blooper2)
+    {
+        blooper.dodge = blooper.dodge_(blooper.def, blooper.spd);
+        blooper2.dodge = blooper2.dodge_(blooper2.def, blooper2.spd);
+        
+        if (blooper.accuracy > blooper2.dodge)
+        {
+            blooper.damage =rand.nextInt(1 +(blooper.strength/4))+1;
+            blooper2.block = blooper2.block_(blooper2.def, blooper2.strength);
+            int truedamage = (blooper.damage - blooper2.block);
+            if (truedamage <= 0)
+                truedamage = 0;
+            blooper2.hp -= truedamage;
+            System.out.println(blooper.name+" did"+ truedamage +" damage!");
+            System.out.println(blooper2.name+"Blocked"+ blooper2.block +" damage!");
+        }
+        else
+        
+        {
+            System.out.println("damage was NOT done!");
+        }
+        
+        return 0;
+    }
+    void taketurn(Fighter blooper, Fighter blooper2)
+    {
+        while (inCombat){
+            blooper.spdcountdown += blooper.spd;
+            blooper2.spdcountdown += blooper2.spd;
+            if (blooper.spdcountdown >= 20 && blooper.spdcountdown> blooper2.spdcountdown)
+            {
+                System.out.println(blooper.name +" attacked!");
+                attack(blooper, blooper2);
+                blooper.spdcountdown = 0;
+                s.nextLine();
+                
+            }
+            else if (blooper2.spdcountdown >= 20 && blooper2.spdcountdown> blooper.spdcountdown)
+            {
+                System.out.println(blooper2.name +"attacked!");
+                attack(blooper2, blooper);
+                blooper2.spdcountdown = 0;
+                s.nextLine();
+            }
+            else
+            {
+                System.out.println("did not attack");
+            
+            }
+            if (blooper2.hp <= 0)
+            {
+                System.out.print(blooper.name +"Wins");
+                inCombat = false;
+            }
+            else if (blooper.hp <= 0)
+            {
+                System.out.println(blooper2.name +" WONMS");   
+                inCombat = false;
+            }
+            System.out.println(blooper.name + "has" + blooper.hp+ "left");
+            System.out.println(blooper2.name + "has" + blooper2.hp+ "left");
+            }
+            }
+        }
+    
+    
+    
+
+
+
+
+
+class Fightgame{
+
+    Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args)
+{
+    Fighter Optimus = new Fighter();
+    Fighter Ebolber = new Fighter();
+    Ebolber.spd = 10;
+    Ebolber.accuracy = 10;
+    Ebolber.strength = 10;
+    Ebolber.name = "EVO-LUTION";
+    Optimus.name = "PrimmCess";
+    Optimus.accuracy = 5;
+    Optimus.strength = 20;
+    Optimus.def = 5;
+    combat combat= new combat();
+    combat.taketurn(Optimus, Ebolber);
+    
+    System.out.println(combat);
+}
+    
+}
